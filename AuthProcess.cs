bc359ef19f49fdac29b21a6a1b452e8b26487fcd
Anduin Xue <anduin@aiursoft.com>
2017-10-20 17:40:49 +0800

    During users sign in, system will update his localization settings in all applications.

--- AuthProcess.cs
+++ AuthProcess.cs
@@ -4,17 +4,21 @@ using AiursoftBase.Services.ToAPIServer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Localization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Http;
+using System;
 
 namespace AiursoftBase
 {
     public class AuthProcess
     {
-        public static async Task<T> AuthApp<T>(Controller _controller, AuthResultAddressModel model, UserManager<T> _userManager, SignInManager<T> _signInManager) where T : AiurUserBase, new()
+        public static async Task<T> AuthApp<T>(Controller controller, AuthResultAddressModel model, UserManager<T> userManager, SignInManager<T> signInManager) where T : AiurUserBase, new()
         {
             var openId = await OAuthService.CodeToOpenIdAsync(model.code, await AppsContainer.AccessToken()());
             var userinfo = await OAuthService.OpenIdToUserInfo(AccessToken: await AppsContainer.AccessToken()(), openid: openId.openid);
 
-            var current = await _userManager.FindByIdAsync(userinfo.User.Id);
+            var current = await userManager.FindByIdAsync(userinfo.User.Id);
             if (current == null)
             {
                 current = new T()
@@ -27,15 +31,24 @@ namespace AiursoftBase
                     PreferedLanguage = userinfo.User.PreferedLanguage,
                     AccountCreateTime = userinfo.User.AccountCreateTime
                 };
-                var result = await _userManager.CreateAsync(current);
+                var result = await userManager.CreateAsync(current);
             }
             else
             {
                 current.Update(userinfo);
-                await _userManager.UpdateAsync(current);
+                await userManager.UpdateAsync(current);
             }
-            await _signInManager.SignInAsync(current, false);
+            await signInManager.SignInAsync(current, false);
+            SetClientLang(controller, userinfo.User.PreferedLanguage);
             return current;
+        }
+
+        public static void SetClientLang(Controller controller, string culture)
+        {
+            controller.HttpContext.Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) });
         }
     }
 }
