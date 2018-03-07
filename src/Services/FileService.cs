8c1e89fb5ffe2c551ca85a798ecbbee3c1d1421e
Anduin Xue <anduin@aiursoft.com>
2018-03-07 12:36:05 +0800

    Using physicalfile to send file to the client.

--- src/Services/FileService.cs
+++ src/Services/FileService.cs
@@ -11,18 +11,18 @@ namespace Aiursoft.Pylon.Services
 {
     public static class FileService
     {
-        public static async Task<FileStreamResult> AiurFile(this ControllerBase controller, string path, string filename, bool download = false)
+        public static async Task<PhysicalFileResult> AiurFile(this ControllerBase controller, string path, string filename, bool download = false)
         {
             var fileInfo = new FileInfo(path);
             var extension = filename.Substring(filename.LastIndexOf('.') + 1);
-            var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, 16 * 1024 * 1024);
+            //var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, 16 * 1024 * 1024);
             if (download)
             {
-                return controller.File(fileStream, MIME.GetContentType(extension, download), filename);
+                return controller.PhysicalFile(path, MIME.GetContentType(extension, download), filename);
             }
             else
             {
-                return controller.File(fileStream, MIME.GetContentType(extension, download));
+                return controller.PhysicalFile(path, MIME.GetContentType(extension, download));
             }
         }
     }
