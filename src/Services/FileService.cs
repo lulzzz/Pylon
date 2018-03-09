ff10aa57fa6a860d2276f2b48c4ee6b91fc6cf13
Anduin Xue <anduin@aiursoft.com>
2018-03-09 16:44:02 +0800

    This may fix the etag caching issue.

--- src/Services/FileService.cs
+++ src/Services/FileService.cs
@@ -17,20 +17,16 @@ namespace Aiursoft.Pylon.Services
         {
             await Task.Delay(0);
             var fileInfo = new FileInfo(path);
-            var fileStream = File.OpenRead(path);
             var extension = filename.Substring(filename.LastIndexOf('.') + 1);
             long etagHash = fileInfo.LastWriteTime.ToUniversalTime().ToFileTime() ^ fileInfo.Length;
             var _etag = '\"' + Convert.ToString(etagHash, 16) + '\"';
             controller.Response.Headers.Add("ETag", _etag);
-            if (controller.Request.Headers.Keys.Contains("If-None-Match") && controller.Request.Headers["If-None-Match"].ToString().Trim('"') == _etag)
+            if (controller.Request.Headers.Keys.Contains("If-None-Match") && controller.Request.Headers["If-None-Match"].ToString().Trim('\"') == _etag)
             {
                 return new StatusCodeResult(304);
             }
             controller.Response.Headers.Add("Content-Length", fileInfo.Length.ToString());
-            //await StreamCopyOperation.CopyToAsync(fileStream, controller.Response.Body, fileInfo.Length, 64 * 1024, controller.HttpContext.RequestAborted);
-            //controller.HttpContext.Abort();
             return controller.PhysicalFile(path, MIME.GetContentType(extension, download));
-            //return controller.File(file, , filename);
         }
     }
 }
